97e22e77beb99e6bd064c7dd1d680fac03f53d57
Camila Gabriela Vera Cortes <133053191+KamiVera2309@users.noreply.github.com>
2023-11-19 20:32:02 -0300

    Mis Plantitas 0.0.3

--- app/src/main/java/com/misplantitas/myapplication/LoginYRegistro.java
+++ app/src/main/java/com/misplantitas/myapplication/LoginYRegistro.java
@@ -2,20 +2,48 @@ package com.misplantitas.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginYRegistro extends AppCompatActivity {
+
+
+    private EditText editTextContraseña;
+    private EditText editTextEmail;
+    private Button btnIrAInicio;
+    private Button btnIrARegistrarse;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginyregistro);
 
+        //Firebase Autenthication
+        mAuth = FirebaseAuth.getInstance();
+
+
+        editTextContraseña = findViewById(R.id.editTextContraseña);
+        editTextEmail = findViewById(R.id.editTextEmail);
+
+        btnIrAInicio = findViewById(R.id.btnIrAInicio);
+        btnIrARegistrarse = findViewById(R.id.btnIrARegistrarse);
+
         Button btnIrARegistrarse = findViewById(R.id.btnIrARegistrarse);
-        btnIrARegistrarse.setOnClickListener(new View.OnClickListener(){
+        btnIrARegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -25,15 +53,48 @@ public class LoginYRegistro extends AppCompatActivity {
             }
         });
 
+
         Button btnIrAInicio = findViewById(R.id.btnIrAInicio);
-        btnIrAInicio.setOnClickListener(new View.OnClickListener(){
+        btnIrAInicio.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getApplicationContext(), Inicio.class);
-                startActivity(intent);
+                // Obtener los valores de correo electrónico y contraseña
+                String email = editTextEmail.getText().toString();
+                String password = editTextContraseña.getText().toString();
 
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    // Ahora puedes realizar el inicio de sesión
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginYRegistro.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        try {
+                                            // El inicio de sesión fue exitoso
+                                            FirebaseUser user = mAuth.getCurrentUser();
+
+                                            // Puedes redirigir a la actividad de inicio o hacer lo que necesites aquí
+                                            // Por ejemplo, redirigir a HomePageActivity
+                                            Intent intent = new Intent(LoginYRegistro.this, Inicio.class);
+                                            startActivity(intent);
+                                        } catch (Exception e) {
+                                            Log.d("InicioSesion", "Observer: " + e);
+                                        }
+
+                                    } else {
+                                        // El inicio de sesión falló, mostrar un mensaje de error
+                                        Toast.makeText(LoginYRegistro.this, "Inicio de sesión fallido", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
+                    // Muestra un mensaje de error indicando que el correo electrónico o la contraseña están vacíos.
+                    Toast.makeText(LoginYRegistro.this, "Correo electrónico o contraseña vacíos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
     }
 }
--- app/src/main/java/com/misplantitas/myapplication/Registro.java
+++ app/src/main/java/com/misplantitas/myapplication/Registro.java
@@ -4,18 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registro extends AppCompatActivity {
 
+    private EditText editTextNombre;
+    private EditText editTextContraseña;
+    private EditText editTextEmail;
+    private Button btnRegistrar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        mAuth = FirebaseAuth.getInstance();
+
         Button btnVolver = findViewById(R.id.btnVolver);
+
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextContraseña = findViewById(R.id.editTextContraseña);
+        editTextEmail = findViewById(R.id.editTextEmail);
+
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
         btnVolver.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LoginYRegistro.class);
@@ -23,6 +46,30 @@ public class Registro extends AppCompatActivity {
             }
         });
 
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String email = editTextEmail.getText().toString();
+                String password = editTextContraseña.getText().toString();
+
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+
+                                        Toast.makeText(Registro.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(Registro.this, LoginYRegistro.class);
+                                        startActivity(intent);
+
+                                    } else {
+                                        Toast.makeText(Registro.this, "Error en el registro", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            });
+        }
     }
-}
+
